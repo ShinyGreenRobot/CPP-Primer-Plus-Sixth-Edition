894bed3747c62c12f7c2cdd210740375c51538fe
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2018-02-17 21:13:18 +0100

    Add a pair of listings from the chapter 2

---
+++ source/chapter_02/listings/listing_02_02_carrots.cpp
@@ -0,0 +1,25 @@
+/**
+ * \file
+ * listing_02_02_carrots.cpp
+ *
+ * \brief
+ * Uses and displays a variable.
+ */
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;			// declare an integer variable
+
+	carrots = 25;			// assign a value to the variable
+	cout << "I have ";
+	cout << carrots;		// display the value of the variable
+	cout << " carrots.";
+	cout << endl;
+	carrots = carrots - 1;	// modify the variable
+	cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	return 0;
+}
