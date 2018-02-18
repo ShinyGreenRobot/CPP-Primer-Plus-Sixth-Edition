fca13c1ae58a589bad94ec7d15fb5d72b1f7c6eb
Henrik Samuelsson <henrik.samuelsson@gmail.com>
2018-02-18 09:15:30 +0100

    Add listing 2.3 and 2.4.

---
+++ source/chapter_02/listings/listing_02_03_getinfo.cpp
@@ -0,0 +1,24 @@
+/**
+ * \file
+ * listing_02_03_getinfo.cpp
+ *
+ * \brief
+ * Input and output.
+ */
+
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int carrots;
+
+    cout << "How many carrots do you have?" << endl;
+	cin >> carrots;    // C++ input
+	cout << "Here are two more. ";
+	carrots = carrots + 2;
+	// the next line concatenates output
+	cout << "Now you have " << carrots << " carrots." << endl;
+	return 0;
+}
---
+++ source/chapter_02/listings/listing_02_04_sqrt.cpp
@@ -0,0 +1,26 @@
+/**
+ * \file
+ * listing_02_04_sqrt.cpp
+ *
+ * \brief
+ * Using the sqrt() function.
+ */
+
+#include <iostream>
+#include <cmath>	// or math.h
+
+int main()
+{
+	using namespace std;
+
+	double area;
+
+    cout << "Enter the floor area, in square feet. of your home: ";
+	cin >> area;
+	double side;
+	side = sqrt(area);
+	cout << "That´s the equivalent of a square " << side
+		 << " feet to side." << endl;
+	cout << "How fascinating!";
+	return 0;
+}
