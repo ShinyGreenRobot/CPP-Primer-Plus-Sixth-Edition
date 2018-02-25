0238c3e017aa75cc51963c361e2687dcd118313a
Shiny Green Robot <henrik.samuelsson@shinygreenrobot.com>
2018-02-25 19:55:54 +0100

    Add listing 4.13.

---
+++ source/chapter_04/listings/listing_04_13_array_struct.cpp
@@ -0,0 +1,33 @@
+/**
+ * \file
+ * listing_04_12_array_struct.cpp
+ *
+ * \brief
+ * An array of structures.
+ */
+#include <iostream>
+
+struct inflatable
+{
+	char name[20];
+	float volume;
+	double price;
+};
+
+int main()
+{
+	using namespace std;
+
+	// initializing an array of structs
+	inflatable guests[2] =
+	{
+		{"Bambi", 0.5, 21.99},        // first structure in array
+		{"Godzilla", 2000, 565.99}    // second structure in array
+	};
+
+	cout << "The guests " << guests[0].name << " and " << guests[1].name
+		 << " have a combined volume of "
+		 << guests[0].volume + guests[1].volume << " cubic feet.\n";
+
+	return 0;
+}
