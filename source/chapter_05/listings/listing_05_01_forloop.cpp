b04eacdcc34ad6ea3ab121bae4cc17e112528d51
Shiny Green Robot <henrik.samuelsson@shinygreenrobot.com>
2018-03-03 18:02:07 +0100

    Add program listing 4.24 and 5.1.

---
+++ source/chapter_05/listings/listing_05_01_forloop.cpp
@@ -0,0 +1,24 @@
+/**
+ * \file
+ * listing_05_01_forloop.cpp
+ *
+ * \brief
+ * Introducing the for loop.
+ */
+#include <iostream>
+int main()
+{
+	using namespace std;
+
+	int i; // create a counter
+
+	// initialize; test ; update
+	for (i = 0; i < 5; i++)
+	{
+		cout << "C++ knows loops.\n";
+	}
+
+	cout << "C++ knows when to stop.\n";
+
+	return 0;
+}
