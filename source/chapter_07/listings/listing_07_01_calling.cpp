9d5b9c3be68016d3297011ab8134d8e3b727770c
Shiny Green Robot <henrik.samuelsson@shinygreenrobot.com>
2018-03-10 08:56:20 +0100

    Add program listing 7.1.

---
+++ source/chapter_07/listings/listing_07_01_calling.cpp
@@ -0,0 +1,31 @@
+/**
+ * \file
+ * listing_07_01_calling.cpp
+ *
+ * \brief
+ * Defining, prototyping, and calling a function.
+ */
+#include <iostream>
+
+void simple();    // function prototype
+
+int main()
+{
+	using namespace std;
+
+	cout << "main() will call the simple() function:\n";
+	simple();    // function call
+	cout << "main() is finished with the simple() function.\n";
+
+	return 0;
+}
+
+/**
+ * \brief
+ * Definition of function that displays a message.
+ */
+void simple()
+{
+	using namespace std;
+	cout << "I'm but a simple function.\n";
+}
